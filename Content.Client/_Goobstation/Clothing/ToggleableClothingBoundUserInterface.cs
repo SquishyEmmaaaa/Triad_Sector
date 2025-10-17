ccbef5f48d87dae0e006b68e7ccaea581f8a6101
sleepyyapril <flyingkarii@gmail.com>
2025-10-17 19:12:26 -0300

    use prototype to avoid disappearing on entity change

--- Content.Client/_Goobstation/Clothing/ToggleableClothingBoundUserInterface.cs
+++ Content.Client/_Goobstation/Clothing/ToggleableClothingBoundUserInterface.cs
@@ -44,6 +44,9 @@ public sealed class ToggleableClothingBoundUserInterface : BoundUserInterface
 
         foreach (var pair in clothing.ClothingUids)
         {
+            if (!EntMan.TryGetComponent(pair.Key, out MetaDataComponent? metaData) || metaData.EntityPrototype == null)
+                continue;
+
             // Change tooltip text if attached clothing is toggle/untoggled
             var attached = clothingContainer.Contains(pair.Key) ? clothing.AttachTooltip : clothing.UnattachTooltip;
             var tooltipText = Loc.GetString(attached);
@@ -51,7 +54,7 @@ public sealed class ToggleableClothingBoundUserInterface : BoundUserInterface
             var action = new RadialMenuActionOption<NetEntity>(SendToggleableClothingMessage, netEntity)
             {
                 ToolTip = tooltipText,
-                IconSpecifier = RadialMenuIconSpecifier.With(pair.Key)
+                IconSpecifier = RadialMenuIconSpecifier.With(metaData.EntityPrototype)
             };
 
             actions.Add(action);
