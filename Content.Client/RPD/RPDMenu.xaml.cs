9030ba48430fbe6286917594a7754823b1356ff6
SquishyEmmaaaa <235807504+SquishyEmmaaaa@users.noreply.github.com>
2026-07-11 06:45:08 -0700

    Thingies?

--- Content.Client/RPD/RPDMenu.xaml.cs
+++ Content.Client/RPD/RPDMenu.xaml.cs
@@ -1,9 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
-// SPDX-FileCopyrightText: 2025 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2026 Triad Sector
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using System.Linq;
 using System.Numerics;
 using Content.Client.RCD;
